2c1a268445966703dd37121063ff58d8565bfcf3
Andrew Ortega <ortegaandrew8@gmail.com>
2026-06-21 02:30:29 -0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+
+        for x in nums:
+            
+            if x in s:
+                return True
+            s.add(x)
+        return False
+            
+        
